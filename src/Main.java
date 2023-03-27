13f556c4845d83e4fdaaef3f0d452a6c3df01cab
Mishator <voyteh89@mail.ru>
2023-03-27 17:59:39 +0300

    Выполнена Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
+    private static Object calculateWhatYear;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        calculateWhatYear (2024);
     }
+
+    public static void calculateWhatYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(year + " год является высокосным");
+        } else {
+            System.out.println(year + " год не является высокосным");
+        }
+     }
 }
